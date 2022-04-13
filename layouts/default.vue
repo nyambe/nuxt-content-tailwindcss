4b572f1b5641d432ccb140cd3c6be68ab6cd9ac1
Samuel Ebuka <samuel.ebuka@gmail.com>
2022-04-13 10:56:58 +0200

    reactivity explained

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full min-h-screen pl-1 pr-5 bg-slate-50 dark:bg-slate-800 md:px-1 prose-pre:dark:bg-slate-100"
+    class="relative w-full min-h-screen pl-1 pr-5 bg-slate-50 dark:bg-slate-800 md:px-1 prose-pre:dark:bg-slate-100"
   >
     <Cabecera />
     <Nuxt />
